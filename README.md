936f6250b1932fbe30eb027d954d4c1971f229b7
numeru55 <89648361+numeru55@users.noreply.github.com>
2022-02-07 23:22:15 +0900

    .

--- README.md
+++ README.md
@@ -4,14 +4,22 @@ https://github.com/mvallim/live-custom-ubuntu-from-scratch
 
 # Overview
 
-This scripts is edited from above.
+Create ubuntu 20.04 focal Japanese live iso.
 
-And is to build focal-jp iso, including Japanese setup, Broadcom WiFi driver and my favorite package.
+Includes Broadcom WiFi driver and my favorite package for my MacBook.
 
 # How to use
 
 ```sh
-./build.sh -
+sudo ./build.sh # "focal.iso" will be created.
 ```
 
+Before build again, clean working directory:
 
+```sh
+sudo rm -rf root image focal.iso
+```
+
+# Bug
+
+`のインストール` icon will be occured on the Desktop. Means ubiquity installer.
--- build.sh
+++ build.sh
@@ -1,203 +1,270 @@
 #!/bin/bash
 
-set -e                  # exit on error
-set -o pipefail         # exit on pipeline error
-set -u                  # treat unset variable as error
-#set -x
+MY_HOST_NAME="focal"
 
-SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
+TARGET_UBUNTU_VERSION="focal"
+TARGET_UBUNTU_MIRROR="http://jp.archive.ubuntu.com/ubuntu/"
 
-CMD=(setup_host debootstrap run_chroot build_iso)
+# OPTION: for new user creation. Uncomment "adduser" in chroot.
 
-DATE=`TZ="UTC" date +"%y%m%d-%H%M%S"`
+# TARGET_USER_NAME="ubuntu"
+# TARGET_USER_PASSWORD="custom"
 
-function help() {
-    # if $1 is set, use $1 as headline message in help()
-    if [ -z ${1+x} ]; then
-        echo -e "This script builds a bootable ubuntu ISO image"
-        echo -e
-    else
-        echo -e $1
-        echo
-    fi
-    echo -e "Supported commands : ${CMD[*]}"
-    echo -e
-    echo -e "Syntax: $0 [start_cmd] [-] [end_cmd]"
-    echo -e "\trun from start_cmd to end_end"
-    echo -e "\tif start_cmd is omitted, start from first command"
-    echo -e "\tif end_cmd is omitted, end with last command"
-    echo -e "\tenter single cmd to run the specific command"
-    echo -e "\tenter '-' as only argument to run all commands"
-    echo -e
-    exit 0
+TIMEZONE="Asia/Tokyo"
+ZONEINFO_FILE="/usr/share/zoneinfo/Asia/Tokyo"
+
+log() {
+  echo "$(date -Iseconds) [info ] $*"
 }
 
-function find_index() {
-    local ret;
-    local i;
-    for ((i=0; i<${#CMD[*]}; i++)); do
-        if [ "${CMD[i]}" == "$1" ]; then
-            index=$i;
-            return;
-        fi
-    done
-    help "Command not found : $1"
+log_error() {
+  echo "$(date -Iseconds) [error] $*" >&2
 }
 
-function chroot_enter_setup() {
-    sudo mount --bind /dev chroot/dev
-    sudo mount --bind /run chroot/run
-    sudo chroot chroot mount none -t proc /proc
-    sudo chroot chroot mount none -t sysfs /sys
-    sudo chroot chroot mount none -t devpts /dev/pts
-}
+# only root can run
+if [[ "$(id -u)" != "0" ]]; then
+  log_error "This script must be run as root"
+  exit 1
+fi
 
-function chroot_exit_teardown() {
-    sudo chroot chroot umount /proc
-    sudo chroot chroot umount /sys
-    sudo chroot chroot umount /dev/pts
-    sudo umount chroot/dev
-    sudo umount chroot/run
-}
+# install packages
 
-function check_host() {
-    local os_ver
-    os_ver=`lsb_release -i | grep -E "(Ubuntu|Debian)"`
-    if [[ -z "$os_ver" ]]; then
-        echo "WARNING : OS is not Debian or Ubuntu and is untested"
-    fi
+apt update
+apt install -y binutils debootstrap squashfs-tools xorriso grub-pc-bin grub-efi-amd64-bin mtools
 
-    if [ $(id -u) -eq 0 ]; then
-        echo "This script should not be run as 'root'"
-        exit 1
-    fi
-}
+# remove directories
 
-# Load configuration values from file
-function load_config() {
-    if [[ -f "$SCRIPT_DIR/config.sh" ]]; then 
-        . "$SCRIPT_DIR/config.sh"
-    elif [[ -f "$SCRIPT_DIR/default_config.sh" ]]; then
-        . "$SCRIPT_DIR/default_config.sh"
-    else
-        >&2 echo "Unable to find default config file  $SCRIPT_DIR/default_config.sh, aborting."
-        exit 1
-    fi
-}
+log "Removing previously created directories ..."
+rm -rf root/
+log "Done."
 
-# Verify that necessary configuration values are set and they are valid
-function check_config() {
-    local expected_config_version
-    expected_config_version="0.4"
+# install base
 
-    if [[ "$CONFIG_FILE_VERSION" != "$expected_config_version" ]]; then
-        >&2 echo "Invalid or old config version $CONFIG_FILE_VERSION, expected $expected_config_version. Please update your configuration file from the default."
-        exit 1
-    fi
-}
+log "Execute debootstrap..."
+mkdir root
+debootstrap --arch=amd64 --variant=minbase focal root
+log "Done."
 
-function setup_host() {
-    echo "=====> running setup_host ..."
-    sudo apt update
-    sudo apt install -y binutils debootstrap squashfs-tools xorriso grub-pc-bin grub-efi-amd64-bin mtools
-    sudo mkdir -p chroot
-}
+# prepare chroot
 
-function debootstrap() {
-    echo "=====> running debootstrap ... will take a couple of minutes ..."
-    sudo debootstrap  --arch=amd64 --variant=minbase $TARGET_UBUNTU_VERSION chroot $TARGET_UBUNTU_MIRROR
-}
+log "Prepare chroot..."
 
-function run_chroot() {
-    echo "=====> running run_chroot ..."
+# mkdir -p root/etc/apt # already exist
 
-    chroot_enter_setup
+cat <<EOF > root/etc/apt/sources.list
+deb $TARGET_UBUNTU_MIRROR $TARGET_UBUNTU_VERSION main restricted universe multiverse
+deb-src $TARGET_UBUNTU_MIRROR $TARGET_UBUNTU_VERSION main restricted universe multiverse
 
-    # Setup build scripts in chroot environment
-    sudo ln -f $SCRIPT_DIR/chroot_build.sh chroot/root/chroot_build.sh
-    sudo ln -f $SCRIPT_DIR/default_config.sh chroot/root/default_config.sh
-    if [[ -f "$SCRIPT_DIR/config.sh" ]]; then
-        sudo ln -f $SCRIPT_DIR/config.sh chroot/root/config.sh
-    fi    
+deb $TARGET_UBUNTU_MIRROR $TARGET_UBUNTU_VERSION-security main restricted universe multiverse
+deb-src $TARGET_UBUNTU_MIRROR $TARGET_UBUNTU_VERSION-security main restricted universe multiverse
 
-    # Launch into chroot environment to build install image.
-    sudo chroot chroot /root/chroot_build.sh -
+deb $TARGET_UBUNTU_MIRROR $TARGET_UBUNTU_VERSION-updates main restricted universe multiverse
+deb-src $TARGET_UBUNTU_MIRROR $TARGET_UBUNTU_VERSION-updates main restricted universe multiverse
+EOF
 
-    # Cleanup after image changes
-    sudo rm -f chroot/root/chroot_build.sh
-    sudo rm -f chroot/root/default_config.sh
-    if [[ -f "chroot/root/config.sh" ]]; then
-        sudo rm -f chroot/root/config.sh
-    fi
 
-    chroot_exit_teardown
-}
+echo $MY_HOST_NAME > root/etc/hostname
 
-function build_iso() {
-    echo "=====> running build_iso ..."
+mount --bind /dev root/dev
+mount --bind /run root/run
 
-    rm -rf image
-    mkdir -p image/{casper,isolinux,install}
+chroot root mount none -t proc /proc
+chroot root mount none -t sysfs /sys
+chroot root mount none -t devpts /dev/pts
+
+chroot root mkdir -p /boot/efi
+chroot root mount ${TARGET_DISK}1 /boot/efi
+chroot root rm -rf /boot/efi/*
+
+log "Done."
+
+# chroot and customize
+log "Start chroot..."
+
+chroot root <<EOT
+
+# export HOME=/root # already set
+export LC_ALL=C
+
+export DEBIAN_FRONTEND=noninteractive
+
+apt-get update
+apt-get install -y systemd-sysv
+# apt-get install -y libterm-readline-gnu-perl systemd-sysv
+
+dbus-uuidgen > /etc/machine-id
+ln -fs /etc/machine-id /var/lib/dbus/machine-id
+
+dpkg-divert --local --rename --add /sbin/initctl
+ln -s /bin/true /sbin/initctl
+
+apt-get upgrade
+
+# live package
+
+apt-get install -y \
+sudo \
+ubuntu-standard \
+casper \
+lupin-casper \
+discover \
+laptop-detect \
+os-prober \
+network-manager \
+resolvconf \
+net-tools \
+wireless-tools \
+grub-common \
+grub-gfxpayload-lists \
+grub-pc \
+grub-pc-bin \
+grub2-common \
+
+#
+
+apt-get install -y ubuntu-minimal
+apt-get install -y ubuntu-desktop-minimal
+
+apt-get install -y --no-install-recommends linux-generic 
+
+# graphic installer - ubiquity
+apt-get install -y \
+ubiquity \
+ubiquity-casper \
+ubiquity-frontend-gtk \
+ubiquity-slideshow-ubuntu \
+ubiquity-ubuntu-artwork
+
+cat <<EOF > /etc/NetworkManager/NetworkManager.conf
+[main]
+rc-manager=resolvconf
+plugins=ifupdown,keyfile
+dns=dnsmasq
+
+[ifupdown]
+managed=false
+EOF
+
+dpkg-reconfigure network-manager
+
+# setup Japanese
+
+echo "Setup Japanese..."
+
+wget -q https://www.ubuntulinux.jp/ubuntu-ja-archive-keyring.gpg -O- | apt-key add -
+wget -q https://www.ubuntulinux.jp/ubuntu-jp-ppa-keyring.gpg -O- | apt-key add -
+wget https://www.ubuntulinux.jp/sources.list.d/focal.list -O /etc/apt/sources.list.d/ubuntu-ja.list
+apt-get update
+apt-get install -y ubuntu-defaults-ja
+
+rm -f /etc/localtime
+ln -s "$ZONEINFO_FILE" /etc/localtime
+echo "$TIMEZONE" > /etc/timezone
+
+update-locale LANG=ja_JP.UTF-8
+sed -i 's/# ja_JP.UTF-8 UTF-8/ja_JP.UTF-8 UTF-8/' /etc/locale.gen
+locale-gen --keep-existing
+
+# install my essential package
+
+sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
+apt-get update
+apt-get install -y google-chrome-stable
+apt-get install -y bcmwl-kernel-source
+apt-get install -y emacs vim bat
+
+apt-add-repository ppa:fish-shell/release-3
+apt-get update
+apt-get install fish
+
+#
+
+truncate -s 0 /etc/machine-id
+
+rm /sbin/initctl
+dpkg-divert --rename --remove /sbin/initctl
+
+umount /proc
+umount /sys
+umount /dev/pts
+
+rm -rf /tmp/* ~/.bash_history
+
+EOT
+
+#####
+
+log "Finished chroot."
+
+umount root/dev
+umount root/run
+
+# /etc/hosts
+
+log "edit /etc/hosts..."
+
+cat <<EOF >>root/etc/hosts
+127.0.1.1 $MY_HOST_NAME
+EOF
+
+# keyboard to jp
+
+log "keyboard to jp..."
+
+cat <<EOF >keyboard
+XKBMODEL=pc105
+XKBLAYOUT=jp
+BACKSPACE=guess
+EOF
+
+mv keyboard root/etc/default
+
+log "Start building ISO..."
+
+rm -rf image
+mkdir -p image/{casper,isolinux,install}
 
     # copy kernel files
-    sudo cp chroot/boot/vmlinuz-**-**-generic image/casper/vmlinuz
-    sudo cp chroot/boot/initrd.img-**-**-generic image/casper/initrd
-
-    # memtest86
-    sudo cp chroot/boot/memtest86+.bin image/install/memtest86+
-
-    wget --progress=dot https://www.memtest86.com/downloads/memtest86-usb.zip -O image/install/memtest86-usb.zip
-    unzip -p image/install/memtest86-usb.zip memtest86-usb.img > image/install/memtest86
-    rm -f image/install/memtest86-usb.zip
+cp root/boot/vmlinuz-**-**-generic image/casper/vmlinuz
+cp root/boot/initrd.img-**-**-generic image/casper/initrd
 
     # grub
-    touch image/ubuntu
-    cat <<EOF > image/isolinux/grub.cfg
+touch image/ubuntu
+cat <<EOF > image/isolinux/grub.cfg
 
 search --set=root --file /ubuntu
 
 insmod all_video
 
 set default="0"
-set timeout=30
+set timeout=10
 
-menuentry "${GRUB_LIVEBOOT_LABEL}" {
+menuentry "${MY_HOST_NAME}" {
    linux /casper/vmlinuz boot=casper nopersistent toram quiet splash ---
    initrd /casper/initrd
 }
 
-menuentry "${GRUB_INSTALL_LABEL}" {
-   linux /casper/vmlinuz boot=casper only-ubiquity quiet splash ---
-   initrd /casper/initrd
-}
-
-menuentry "Check disc for defects" {
-   linux /casper/vmlinuz boot=casper integrity-check quiet splash ---
-   initrd /casper/initrd
-}
-
-menuentry "Test memory Memtest86+ (BIOS)" {
-   linux16 /install/memtest86+
-}
-
-menuentry "Test memory Memtest86 (UEFI, long load time)" {
-   insmod part_gpt
-   insmod search_fs_uuid
-   insmod chain
-   loopback loop /install/memtest86
-   chainloader (loop,gpt1)/efi/boot/BOOTX64.efi
-}
 EOF
 
-    # generate manifest
-    sudo chroot chroot dpkg-query -W --showformat='${Package} ${Version}\n' | sudo tee image/casper/filesystem.manifest
-    sudo cp -v image/casper/filesystem.manifest image/casper/filesystem.manifest-desktop
-    for pkg in $TARGET_PACKAGE_REMOVE; do
-        sudo sed -i "/$pkg/d" image/casper/filesystem.manifest-desktop
-    done
+# Packages to be removed from the target system after installation completes succesfully
+TARGET_PACKAGE_REMOVE="
+    ubiquity \
+    casper \
+    discover \
+    laptop-detect \
+    os-prober \
+"
+
+# generate manifest
+chroot root dpkg-query -W --showformat='${Package} ${Version}\n' | tee image/casper/filesystem.manifest
+cp -v image/casper/filesystem.manifest image/casper/filesystem.manifest-desktop
+for pkg in $TARGET_PACKAGE_REMOVE; do
+    sed -i "/$pkg/d" image/casper/filesystem.manifest-desktop
+done
 
     # compress rootfs
-    sudo mksquashfs chroot image/casper/filesystem.squashfs \
+mksquashfs root image/casper/filesystem.squashfs \
         -noappend -no-duplicates -no-recovery \
         -wildcards \
         -e "var/cache/apt/archives/*" \
@@ -206,11 +273,11 @@ EOF
         -e "tmp/*" \
         -e "tmp/.*" \
         -e "swapfile"
-    printf $(sudo du -sx --block-size=1 chroot | cut -f1) > image/casper/filesystem.size
+printf $(du -sx --block-size=1 root | cut -f1) > image/casper/filesystem.size
 
     # create diskdefines
-    cat <<EOF > image/README.diskdefines
-#define DISKNAME  ${GRUB_LIVEBOOT_LABEL}
+cat <<EOF > image/README.diskdefines
+#define DISKNAME  ${MY_HOST_NAME}
 #define TYPE  binary
 #define TYPEbinary  1
 #define ARCH  amd64
@@ -222,7 +289,7 @@ EOF
 EOF
 
     # create iso image
-    pushd $SCRIPT_DIR/image
+pushd image
     grub-mkstandalone \
         --format=x86_64-efi \
         --output=isolinux/bootx64.efi \
@@ -233,7 +300,7 @@ EOF
     (
         cd isolinux && \
         dd if=/dev/zero of=efiboot.img bs=1M count=10 && \
-        sudo mkfs.vfat efiboot.img && \
+        mkfs.vfat efiboot.img && \
         LC_CTYPE=C mmd -i efiboot.img efi efi/boot && \
         LC_CTYPE=C mcopy -i efiboot.img ./bootx64.efi ::efi/boot/
     )
@@ -249,13 +316,13 @@ EOF
 
     cat /usr/lib/grub/i386-pc/cdboot.img isolinux/core.img > isolinux/bios.img
 
-    sudo /bin/bash -c "(find . -type f -print0 | xargs -0 md5sum | grep -v -e 'md5sum.txt' -e 'bios.img' -e 'efiboot.img' > md5sum.txt)"
+    /bin/bash -c "(find . -type f -print0 | xargs -0 md5sum | grep -v -e 'md5sum.txt' -e 'bios.img' -e 'efiboot.img' > md5sum.txt)"
 
-    sudo xorriso \
+    xorriso \
         -as mkisofs \
         -iso-level 3 \
         -full-iso9660-filenames \
-        -volid "$TARGET_NAME" \
+        -volid "$MY_HOST_NAME" \
         -eltorito-boot boot/grub/bios.img \
         -no-emul-boot \
         -boot-load-size 4 \
@@ -269,7 +336,7 @@ EOF
         -append_partition 2 0xef isolinux/efiboot.img \
 	-appended_part_as_gpt  \
 	--mbr-force-bootable  \
-        -output "$SCRIPT_DIR/$TARGET_NAME.iso" \
+        -output "../$MY_HOST_NAME.iso" \
         -m "isolinux/efiboot.img" \
         -m "isolinux/bios.img" \
         -graft-points \
@@ -278,45 +345,5 @@ EOF
            "."
 
     popd
-}
 
-# =============   main  ================
-
-# we always stay in $SCRIPT_DIR
-cd $SCRIPT_DIR
-
-load_config
-check_config
-check_host
-
-# check number of args
-if [[ $# == 0 || $# > 3 ]]; then help; fi
-
-# loop through args
-dash_flag=false
-start_index=0
-end_index=${#CMD[*]}
-for ii in "$@";
-do
-    if [[ $ii == "-" ]]; then
-        dash_flag=true
-        continue
-    fi
-    find_index $ii
-    if [[ $dash_flag == false ]]; then
-        start_index=$index
-    else
-        end_index=$(($index+1))
-    fi
-done
-if [[ $dash_flag == false ]]; then
-    end_index=$(($start_index + 1))
-fi
-
-#loop through the commands
-for ((ii=$start_index; ii<$end_index; ii++)); do
-    ${CMD[ii]}
-done
-
-echo "$0 - Initial build is done!"
-
+log "Finished script."
